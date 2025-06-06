49aa34ac8fa4631f115cbe520842f6ff21f6534b
mtt2701 <weizijing@gpu-with-vm-2.c.steel-pod-458521-q6.internal>
2025-06-06 08:43:16 +0000

    eval results on human and bot0 summary

---
+++ server-gpu/tweet_eval.py
@@ -0,0 +1,136 @@
+from nltk.translate.bleu_score import sentence_bleu, SmoothingFunction
+from rouge_score import rouge_scorer
+import bert_score
+import json
+import boto3
+from dotenv import load_dotenv
+import os
+from botocore.exceptions import ClientError
+import evaluate
+from statistics import mean
+
+load_dotenv()
+s3 = boto3.client('s3', region_name='us-west-1')
+
+rouge = evaluate.load("rouge")
+bertscore = evaluate.load("bertscore")
+
+def eval_human():
+    with open("data/all_threads.json", "r") as f:
+        data = json.load(f)
+    threads = [(item["paper"].split('/')[-1], ''.join(item["tweets"])) for item in data["threads"]]
+    references = []
+    predictions = []
+    rouge_results = []
+
+    for paper_id, tweet in threads:
+        print(f"Curr paper_id = {paper_id}")
+        paper_text = ''
+        try:
+            obj = s3.get_object(Bucket=os.environ.get('BUCKET_NAME'), Key=f'papers/threads/{paper_id}.txt')
+            paper_text = obj['Body'].read().decode('utf-8')
+            rouge_result = rouge.compute(predictions=[tweet], references=[paper_text])
+            rouge_results.append({"paper_id": paper_id, **rouge_result})
+            references.append(paper_text)
+            predictions.append(tweet)
+        except ClientError as e:
+            print(f"Error getting object papers/{paper_id}.txt: {e}")
+            continue
+    
+    bert_results = bertscore.compute(predictions=predictions, references=references, lang="en")
+    combined_results = []
+
+    for i in range(len(rouge_results)):
+        combined = {
+            "paper_id": rouge_results[i]["paper_id"],
+            "rouge1": rouge_results[i]["rouge1"],
+            "rouge2": rouge_results[i]["rouge2"],
+            "rougeL": rouge_results[i]["rougeL"],
+            "rougeLsum": rouge_results[i]["rougeLsum"],
+            "bertscore_precision": bert_results["precision"][i],
+            "bertscore_recall": bert_results["recall"][i],
+            "bertscore_f1": bert_results["f1"][i],
+        }
+        combined_results.append(combined)
+    
+    # Get all metric names except 'index'
+    metric_keys = [k for k in combined_results[0] if k != "paper_id"]
+
+    # Compute mean for each metric
+    metric_means = {
+        metric: mean(result[metric] for result in combined_results)
+        for metric in metric_keys
+    }
+
+    results = {"method": "human", "scores": combined_results, "means": metric_means}
+    with open("eval_results/human.json", "w") as f:
+        json.dump(results, f, indent=2)
+
+def eval_Qwen3_4B(num_bot, label):
+    with open("data/all_threads.json", "r") as f:
+        data = json.load(f)
+    threads = [item["paper"].split('/')[-1] for item in data["threads"]]
+    references = []
+    predictions = []
+    rouge_results = []
+
+    for paper_id in threads:
+        print(f"Curr paper_id = {paper_id}")
+        paper_text = ''
+        try:
+            obj = s3.get_object(Bucket=os.environ.get('BUCKET_NAME'), Key=f'papers/threads/{paper_id}.txt')
+            paper_text = obj['Body'].read().decode('utf-8')
+            tweet = ''
+            path = f"thread_downloads/bot{num_bot}/{paper_id}.json"
+
+            if os.path.exists(path):
+                with open(path, "r") as f:
+                    data = json.load(f)
+                    if (label == "bad_summary"):
+                        if data["best_summary_idx"] < 0:
+                            continue
+                        tweet = data["all_summaries"][1 - data["best_summary_idx"]]
+                    else:
+                        tweet = data[label]
+                    rouge_result = rouge.compute(predictions=[tweet], references=[paper_text])
+                    rouge_results.append({"paper_id": paper_id, **rouge_result})
+                    predictions.append(tweet)
+                references.append(paper_text)
+
+        except ClientError as e:
+            print(f"Error getting object papers/{paper_id}.txt: {e}")
+            continue
+    
+    bert_results = bertscore.compute(predictions=predictions, references=references, lang="en")
+    combined_results = []
+
+    for i in range(len(rouge_results)):
+        combined = {
+            "paper_id": rouge_results[i]["paper_id"],
+            "rouge1": rouge_results[i]["rouge1"],
+            "rouge2": rouge_results[i]["rouge2"],
+            "rougeL": rouge_results[i]["rougeL"],
+            "rougeLsum": rouge_results[i]["rougeLsum"],
+            "bertscore_precision": bert_results["precision"][i],
+            "bertscore_recall": bert_results["recall"][i],
+            "bertscore_f1": bert_results["f1"][i],
+        }
+        combined_results.append(combined)
+    
+    # Get all metric names except 'index'
+    metric_keys = [k for k in combined_results[0] if k != "paper_id"]
+    # Compute mean for each metric
+    metric_means = {
+        metric: mean(result[metric] for result in combined_results)
+        for metric in metric_keys
+    }
+    results = {"method": f"bot{num_bot}_{label}_Qwen3_4B", "scores": combined_results, "means": metric_means}
+
+    with open(f"eval_results/bot{num_bot}_{label}_Qwen3_4B.json", "w") as f:
+        json.dump(results, f, indent=2)
+
+def main():
+    eval_Qwen3_4B(0, "bad_summary")
+
+if __name__ == "__main__":
+    main()
