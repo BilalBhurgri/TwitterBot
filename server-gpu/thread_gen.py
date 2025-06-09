c2a1b77f169cbaaaab6cba721d42774a88f9a2b0
mtt2701 <weizijing@gpu-with-vm-2.c.steel-pod-458521-q6.internal>
2025-06-09 05:17:45 +0000

    threads from llama and mistral

--- server-gpu/thread_gen.py
+++ server-gpu/thread_gen.py
@@ -13,17 +13,26 @@ project_root = str(Path(__file__).parent.parent)
 sys.path.append(project_root)
 
 from try_models.summary_to_tweet import (
-    generate_tweet_qwen
+    generate_tweet_qwen,
+    generate_tweet_mistral,
+    generate_tweet_llama
 )
 
 load_dotenv()
 s3 = boto3.client('s3', region_name='us-west-1')
 
-tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen3-4B")
+model_name = "meta-llama/Llama-3.1-8B-Instruct"
+quantization_config = BitsAndBytesConfig(
+    load_in_8bit=True,
+    llm_int8_threshold=6.0,  # Default threshold
+)
+
+tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForCausalLM.from_pretrained(
-            "Qwen/Qwen3-4B",
-            torch_dtype=torch.float16,
-            device_map="auto"
+            model_name,
+            quantization_config=quantization_config,
+            device_map="auto",
+            low_cpu_mem_usage=True
         )
 
 def thread_gen():
@@ -32,7 +41,7 @@ def thread_gen():
     paper_ids = [item["paper"].split('/')[-1] for item in data["threads"]]
     for paper_id in paper_ids:
         try:
-            path = f"downloads/thread_downloads/Qwen3-4B/bot1/{paper_id}.json"
+            path = f"downloads/thread_downloads/Llama-3.1-8B-Instruct/bot0/{paper_id}.json"
             if os.path.exists(path):
                 with open(path, "r") as f:
                     result = json.load(f)
@@ -41,14 +50,14 @@ def thread_gen():
                 tweets = []
                 for i in range(min([len(sentences), 8])):
                     print(f"Sentence: {sentences[i]}")
-                    tweet = generate_tweet_qwen(sentences[i], tokenizer, model, bot_num=1)
+                    tweet = generate_tweet_llama(sentences[i], tokenizer, model, bot_num=0)
                     real_tweet = tweet.split('\n')[0]
                     if i == 0:
                         real_tweet += f"\n Link: https://arxiv.org/abs/{paper_id}"
                     print(f"Tweet: {real_tweet}")
                     tweets.append(real_tweet)
                 output = {"thread": tweets}
-                with open(f'threads/Qwen3-4B/bot1/{paper_id}.json', 'w') as f:
+                with open(f'threads/Llama-3.1-8B-Instruct/bot0/{paper_id}.json', 'w') as f:
                     json.dump(output, f, indent=2)
         except ClientError as e:
             print(f"Error getting object {paper_id}.txt: {e}")
